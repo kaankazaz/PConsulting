51ab232614fd803e17e94e7fe30508d2e5f2bf46
Bilge Kaan KAZAZ <bkkazaz@kazaz.com.tr>
2019-11-17 16:40:29 +0300

    - Project initialized - Custom cuthorization implemented

---
+++ PConsulting/PConsulting/Data/BOL/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PConsulting.Data.BOL
+{
+    // Added for extending the default IdentityUser class for possible future requests - Kaan KAZAZ
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
---
+++ PConsulting/PConsulting/Startup.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PConsulting.Data;
+using PConsulting.Data.BOL;
+using System;
+using System.Threading.Tasks;
+
+namespace PConsulting
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            //Added for Password Strength Setting - Kaan KAZAZ
+            services.Configure<IdentityOptions>(options =>
+            {
+                //Password settings
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 6; //Kept short for easy demonstration - Kaan KAZAZ
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+
+                //Lockout settings
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+                //options.Lockout.MaxFailedAccessAttempts = 10; //Commented out for easy demonstration - Kaan KAZAZ
+                options.Lockout.AllowedForNewUsers = true;
+
+                //User settings
+                options.User.RequireUniqueEmail = true;
+            });
+
+            //Added for implementing authentication - Kaan KAZAZ
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders()
+                .AddDefaultUI(UIFramework.Bootstrap4);
+
+
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            //services.AddDefaultIdentity<IdentityUser>()
+            //    .AddDefaultUI(UIFramework.Bootstrap4)
+            //    .AddEntityFrameworkStores<ApplicationDbContext>(); //Removed default idendity code as custom identity is implemented above - Kaan KAZAZ
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline. Modified to include IServiceProvider - Kaan KAZAZ
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider services)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            CreatePublisherRole(services).Wait(); //Kaan KAZAZ
+        }
+
+
+        //Creates an Publisher role and assigns it to the first user for demonstration - Kaan KAZAZ
+        private async Task CreatePublisherRole(IServiceProvider serviceProvider)
+        {
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            IdentityResult roleResult;
+
+            //Check and add if Publisher role and a user exists - Kaan KAZAZ
+            var roleCheck = await RoleManager.RoleExistsAsync("Publisher");
+            var userCount = await UserManager.Users.CountAsync();
+
+            if (!roleCheck && userCount > 0)
+            {
+                roleResult = await RoleManager.CreateAsync(new IdentityRole("Publisher"));
+
+                //Assign the newly created Publisher role to the first user (if there is a user) - Kaan KAZAZ
+                ApplicationUser user = await UserManager.Users.FirstOrDefaultAsync();
+
+                if (user != null)
+                {
+                    await UserManager.AddToRoleAsync(user, "Publisher");
+                }
+            }
+
+
+        }
+    }
+}
